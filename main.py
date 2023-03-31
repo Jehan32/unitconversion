3cec3e4f80fae1df1b1bee82f9f505dfd50ea000
Jehan32 <jehanshekhaji@gmail.com>
2023-03-31 13:30:31 +0530

    help commands

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
-
-print("this is unitconversion")
+print("this is unit conversion")
 print("in this u can can convert the 'cm' to 'm'  'm' to 'km'etc.")
+print("0 to convert 'cm' to 'in'")
+print("1 to convert 'm' to 'ft'")
+print("2 to convert 'km' to 'mile'")
+
+
+
 
 if __name__ == "__main__":
     main()
